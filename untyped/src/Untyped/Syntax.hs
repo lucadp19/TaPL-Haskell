c385325163a6b76c65ebf53a030ddce4ca0ba09f
lucadp19 <luca.dp1909@gmail.com>
2021-01-02 23:21:24 +0100

    Added syntax

---
+++ untyped/src/Untyped/Syntax.hs
@@ -0,0 +1,10 @@
+module Untyped.Syntax where
+
+import qualified Data.Text as T
+
+-- | A Term of the Untyped Lambda Calculus.
+data Term
+    = Var Int           -- ^ A variable, represented by its De Bruijn index.
+    | Lam T.Text Term   -- ^ A lambda abstraction with the parameter name.
+    | App Term Term     -- ^ A function application.
+    deriving (Eq, Show) 
