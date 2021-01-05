e6ba4990c4d33d45fa626d4f5c654a859b78e4df
lucadp19 <luca.dp1909@gmail.com>
2021-01-06 00:42:39 +0100

    Removed Eval monad from computations, since we don't need an environment

--- untyped/src/Untyped/Eval.hs
+++ untyped/src/Untyped/Eval.hs
@@ -1,16 +1,15 @@
 {-# LANGUAGE LambdaCase #-}
 module Untyped.Eval 
-    ( step
+    ( -- * Evaluators
+      -- ** Single step
+      step
+      -- ** Multistep
     , eval
     ) where
 
-
-import Untyped.Monad
-import Untyped.Syntax
+import Untyped.Syntax ( Term(..) )
 import Untyped.Environment ( insertIntoLocals )
 
-import Control.Monad.Trans.Maybe
-import Control.Monad.Reader
 
 -- | Checks if a given term is a value: a term is a value if and only if it's a single lambda abstraction.
 isValue :: Term -> Bool
@@ -21,46 +20,48 @@ isValue = \case
 -- | The shifting function: given an @Int@ and a @Term@ the function modifies the term shifting it into the right context.
 shift :: Int -> Term -> Term
 shift d = go 0
-    where 
-        go :: Int -> Term -> Term
-        go c = \case
-            Var k      -> Var $ if k >= c then k+d else k
-            Lam name t -> Lam name $ go (c+1) t
-            App t1 t2  -> App (go c t1) (go c t2) 
+  where 
+    go :: Int -> Term -> Term
+    go c = \case
+        Var k      -> Var $ if k >= c then k+d else k
+        Lam name t -> Lam name $ go (c+1) t
+        App t1 t2  -> App (go c t1) (go c t2) 
 
 -- | The substitution function.
 subst :: Int -> Term -> Term -> Term
 subst j s = go 0
-    where 
-        go :: Int -> Term -> Term
-        go c = \case
-            Var k      -> if k == j+c then shift c s else Var k
-            Lam name t -> Lam name $ go (c+1) t
-            App t1 t2  -> App (go c t1) (go c t2) 
+  where 
+    go :: Int -> Term -> Term
+    go c = \case
+        Var k      -> if k == j+c then shift c s else Var k
+        Lam name t -> Lam name $ go (c+1) t
+        App t1 t2  -> App (go c t1) (go c t2) 
 
 -- | The beta-reduction function: substitutes a @Term@ into the body of a lambda abstraction.
 substTop :: Term    -- ^ The term to be substituted into the body of the lambda abstraction.
          -> Term    -- ^ The body of the lambda abstraction.
-         -> Eval Term
-substTop s t = pure $ shift (-1) $ subst 0 (shift 1 s) t
+         -> Term
+substTop s t = shift (-1) $ subst 0 (shift 1 s) t
 
 
--- | Steps an expression into another expression. 
--- If there is no applicable rule, it returns nothing.
-step :: Term -> MaybeT Eval Term
+{- | 
+Steps an expression into another expression. 
+If there is no applicable rule, it returns nothing.
+-}
+step :: Term -> Maybe Term
 step (App (Lam name t1) t2) 
-    | isValue t2 = lift . local (insertIntoLocals name) $ substTop t2 t1
-    | otherwise  = App <$> pure (Lam name t1) <*> step t2
+    | isValue t2 = pure $ substTop t2 t1
+    | otherwise  = App (Lam name t1) <$> step t2
 step (App t1 t2) = App <$> step t1 <*> pure t2
-step _           = mzero
+step _           = Nothing
 
 -- | Evaluates a term to a value or to a stuck term, repeatedly using the small-step semantic given. 
-multistep :: Term -> Eval Term
-multistep t = runMaybeT (step t) >>= \case
-                Just t' -> multistep t'
-                Nothing -> pure t
+multistep :: Term -> Term
+multistep t = case step t of
+    Just t' -> multistep t'
+    Nothing -> t
 
 -- | Evaluates a term to a value or to a stuck term, repeatedly using the small-step semantic given. 
-eval :: Term -> Eval Term
+eval :: Term -> Term
 eval = multistep
 
